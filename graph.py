787d7b8269471aa968060b7e43bea69714ad728a
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-07-04 09:06:22 -0700

    Had a more understanding on making bars and a pie graph

--- graph.py
+++ graph.py
@@ -1,11 +1,27 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
 import matplotlib.pyplot as plt
+from pylab import rcParams
+import seaborn as sns
 
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 6, 8, 10]
+rcParams['figure.figsize'] = 8,4
+sns.set_style('whitegrid')
 
-plt.plot(x, y)
+
+x = range(1, 10)
+y = [1,2,3,4,0.5,4,3,2,1]
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "gray", "black"]
+plt.bar(x, y, color=colors)
 plt.title("Simple Line Plot")
 plt.xlabel("X values")
 plt.ylabel("Y values")
 
+plt.show()
+
+z = [1,2,3,4,0.5]
+veh_type = ['bicycle', 'motorcycle', 'car', 'van', 'stroller']
+plt.pie(z, labels=veh_type)
 plt.show()
